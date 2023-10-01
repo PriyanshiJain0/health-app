2f9a9dc02cf632882af4c63658cebc39ca35e3c5
PriyanshiJain0 <116676616+PriyanshiJain0@users.noreply.github.com>
2023-10-01 16:31:01 +0000

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+function App() {
+
+      // sidebar
+      // main
+      // footer
+      
+  return (
+    <div className="health-protal-wrapper">
+      <header className="app-header border-bottom">
+        Health Portal
+      </header>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,13 @@
+ const useFetch = () => {
+
+   // create states
+   // call api
+   // update state based on res
+
+    
+
+    return 
+ }
+
+
+ export default useFetch;
